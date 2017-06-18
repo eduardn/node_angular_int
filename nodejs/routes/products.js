1f37b4275629020be2d8761a162136e491a80d12
eduardn <neculaesi.eduard@gmail.com>
2017-06-18 09:52:58 +0300

    Product item page

--- nodejs/routes/products.js
+++ nodejs/routes/products.js
@@ -18,7 +18,7 @@ router.get('/:id', function (req, res, next) {
         return;
     }
 
-    res.render('index', { product });
+    res.render('products/item', { product });
 });
 
 module.exports = router;
